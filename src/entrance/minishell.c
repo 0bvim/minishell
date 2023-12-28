f4508f04dcb6e631cee8c7ba37309b252ccfea41
nivicius <niviciusdev@gmail.com>
2023-12-28 12:00:03 -0300

    Update main function updated and with norminette

--- src/entrance/minishell.c
+++ src/entrance/minishell.c
@@ -3,21 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 21:19:43 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/18 21:34:29 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/12/28 11:59:16 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argc __attribute__((unused)), char **argv __attribute__((unused)),
+		char **envp)
 {
-	(void)argc;
-	(void)argv;
-	(void)envp;
 	printf("Hello I'm your own shell!\n");
-
 	return (EXIT_SUCCESS);
 }
