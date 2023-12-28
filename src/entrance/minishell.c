23ff5669b3a7c35a6d7659aa427e8f11bc8bb4dc
nivicius <niviciusdev@gmail.com>
2023-12-28 13:28:59 -0300

    Updated the guide to do it!

--- src/entrance/minishell.c
+++ src/entrance/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 21:19:43 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/28 11:59:16 by nivicius         ###   ########.fr       */
+/*   Updated: 2023/12/28 13:28:50 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 int	main(int argc __attribute__((unused)), char **argv __attribute__((unused)),
 		char **envp)
 {
+	/* display the prompt */
+	/* read user input */
+	/* parser user input into commands and expand variables if have one. */
+	/* search the command */
+	/* create a child process (fork) */
+	/* execute the command(s) (execv)*/
+	/* wait for command completion */
+	/* need to finish this little guide */
 	printf("Hello I'm your own shell!\n");
 	return (EXIT_SUCCESS);
 }
