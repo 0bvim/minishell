3f6b08ce111b75af2bbeb3a7d890a330009fa69e
nivicius <niviciusdev@gmail.com>
2023-12-31 01:28:55 -0300

    fix: updated quotes function, need to handle errors

--- src/parser/parentheses.c
+++ src/parser/parentheses.c
@@ -6,52 +6,52 @@
 /*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:19:08 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/30 21:46:20 by nivicius         ###   ########.fr       */
+/*   Updated: 2023/12/31 01:26:49 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void tokenize_parentheses(char *p, t_token *tokens, int *num)
+void tokenize_parentheses(char **p, t_token **tokens, int *num)
 {
-	if (*p == '(')
+	if (**p == '(')
 	{
-		tokens[*num].type = TOKEN_LPAREN;
-		tokens[*num].value = "(";
+		(*tokens)[*num].type = TOKEN_LPAREN;
+		(*tokens)[*num].value = "(";
 		(*num)++;
-		p++;
+		(*p)++;
 	}
-	else if (*p == ')')
+	else if (**p == ')')
 	{
-		tokens[*num].type = TOKEN_RPAREN;
-		tokens[*num].value = ")";
+		(*tokens)[*num].type = TOKEN_RPAREN;
+		(*tokens)[*num].value = ")";
 		(*num)++;
-		p++;
+		(*p)++;
 	}
 }
 
-void tokenize_quote(char *p, t_token *tokens, int *num)
+void tokenize_quote(char **p, t_token **tokens, int *num)
 {
-	if (*p == '"' || *p == '\'')
+	char quote;
+	char *begin;
+	int quote_count;
+
+	if (**p == '"' || **p == '\'')
 	{
-		char quote = *p;
-		char *begin = ++p;
-		int quoteCount = 1;
-		while (*p && quoteCount > 0)
+		quote = **p;
+		begin = ++(*p);
+		quote_count = 1;
+		while (**p && quote_count > 0)
 		{
-			if (*p == quote)
-				quoteCount++;
-			if (*p == quote)
-				quoteCount--;
-			p++;
+			if (**p == quote)
+				quote_count++;
+			(*p)++;
 		}
-		if (quoteCount != 0)
-		{
-			// Handle error: unmatched quote
-		}
-		tokens[*num].type = TOKEN_QUOTE;
-		tokens[*num].value = ft_strndup(begin, p - begin);
+		if (quote_count % 2 != 0)
+			printf("not match quotes\n");
+		(*tokens)[*num].type = TOKEN_QUOTE;
+		(*tokens)[*num].value = ft_strndup(begin, (*p - begin));
 		(*num)++;
-		p++;
+		(*p)++;
 	}
 }
--- src/parser/tokenize.c
+++ src/parser/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:12:23 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/30 23:55:27 by nivicius         ###   ########.fr       */
+/*   Updated: 2023/12/31 00:44:45 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ t_token *tokenize(char *input)
 		tokenize_space(&p);
 		tokenize_and(&p, &tokens, &num_tokens);
 		tokenize_or(&p, &tokens, &num_tokens);
-		tokenize_parentheses(p, tokens, &num_tokens);
-		tokenize_quote(p, tokens, &num_tokens);
+		tokenize_parentheses(&p, &tokens, &num_tokens);
+		tokenize_quote(&p, &tokens, &num_tokens);
 		tokenize_argument(&p, &tokens, &num_tokens);
 	}
 	return (tokens);
