8414d0d3c537c9435ba0dcc4c8942715dd6dbc82
vde-frei <vde-frei@student.42sp.org.br>
2024-01-05 13:56:04 -0300

    fix: token_or behavior

--- src/tokenizer/and_or.c
+++ src/tokenizer/and_or.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   and_or.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:18:23 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/30 23:56:02 by nivicius         ###   ########.fr       */
+/*   Updated: 2024/01/05 13:55:04 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,16 @@ void tokenize_and(char **p, t_token **tokens, int *num)
 
 void tokenize_or(char **p, t_token **tokens, int *num)
 {
-	if (**p == '|' && *(*p + 1) == '|')
+	if (**p == '|' && *(*p + 1) == '|' && *(*p + 2) != '|')
 	{
 		(*tokens)[*num].type = TOKEN_OR;
 		(*tokens)[*num].value = "||";
 		(*num)++;
 		*p += 2;
+	}
+	else if ((**p == '|' && *(*p + 1) != '|') || (*(*p + 2) == '|'))
+	{
+		(*p)++;
+		ft_putstr_fd("Sintax error\n", STDERR_FILENO);
 	}
 }
