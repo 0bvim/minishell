c97d843405a209a52507739175f866f06dd3d98c
nivicius <niviciusdev@gmail.com>
2024-01-13 07:19:01 -0300

    fix unused variable on main

--- src/entrance/minishell.c
+++ src/entrance/minishell.c
@@ -10,34 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
-	/* display the prompt */
-	/* read user input */
-	/* parser user input into commands and expand variables if have one. */
-	/* search the command */
-	/* create a child process (fork) */
-	/* execute the command(s) (execv)*/
-	/* wait for command completion */
-	/* need to finish this little guide */
-	/* remember to use extern **environ */
-int	main(void)
-{
-	char	*input;
-	t_list	*tokens;
+#include "../../includes/minishell.h"
+/* display the prompt */
+/* read user input */
+/* parser user input into commands and expand variables if have one. */
+/* search the command */
+/* create a child process (fork) */
+/* execute the command(s) (execv)*/
+/* wait for command completion */
+/* need to finish this little guide */
+/* remember to use extern **environ */
+int main(void) {
+  char *input;
+  t_list *tokens;
 
-	clear_console();
-	while (true)
-	{
-		input = readline("minishell~$ ");
-		if (input != NULL && ft_strncmp(input, "exit", 4))
-		{
-			//printf("%s\n", input);
-			tokens = tokenizer(input);
-			free (input);
-		}
-		else
-			break;
-	}
-	free(input);
-	return (EXIT_SUCCESS);
+  clear_console();
+  while (true) {
+    input = readline("minishell~$ ");
+    if (input != NULL && ft_strncmp(input, "exit", 4)) {
+      // printf("%s\n", input);
+      tokens = tokenizer(input);
+      (void)tokens;
+      free(input);
+    } else
+      break;
+  }
+  free(input);
+  return (EXIT_SUCCESS);
 }
