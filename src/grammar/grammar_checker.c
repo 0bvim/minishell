fa7461aeef454f0940e4e904cfff63c498ef44b9
Bruno Moretti <bmoretti@c2r2p4.42sp.org.br>
2024-01-15 18:41:18 -0300

    skip parenthesis

--- src/grammar/grammar_checker.c
+++ src/grammar/grammar_checker.c
@@ -6,11 +6,21 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:37:13 by bmoretti          #+#    #+#             */
-/*   Updated: 2024/01/15 17:10:00 by bmoretti         ###   ########.fr       */
+/*   Updated: 2024/01/15 18:39:28 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static void	skip_parenthesis(t_element	**el)
+{
+	if (((t_token *)(*el)->content)->type == L_PAREN)
+	{
+		while (((t_token *)(*el)->content)->type != R_PAREN)
+			*el = (*el)->next;
+		*el = (*el)->next;
+	}
+}
 
 void	grammar_checker(t_list *tokens)
 {
@@ -21,6 +31,7 @@ void	grammar_checker(t_list *tokens)
 	el = tokens->first;
 	while (el)
 	{
+		skip_parenthesis(&el);
 		redir_and_or_pipe_rule(el);
 		el = el->next;
 	}
