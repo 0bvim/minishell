7ac874996a64a79e19d1827af32d8372a2b3a8f5
nivicius <niviciusdev@gmail.com>
2024-02-08 02:04:31 -0300

    fixed redirect files creation

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brmoretti <brmoretti@student.42.fr>        +#+  +:+       +#+        */
+/*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 20:55:27 by vde-frei          #+#    #+#             */
-/*   Updated: 2024/02/05 13:38:35 by brmoretti        ###   ########.fr       */
+/*   Updated: 2024/02/08 01:15:45 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,7 @@ typedef struct s_ast
 {
 	int					type;
 	int					type_prev;
+	int					success;
 	struct s_ast		*left;
 	struct s_ast		*right;
 	t_list				*exec;
--- src/exec/handling_redirs.c
+++ src/exec/handling_redirs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handling_redirs.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:26:07 by vde-frei          #+#    #+#             */
-/*   Updated: 2024/02/05 16:18:33 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/02/08 01:51:18 by nivicius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,29 @@ static int	append_trunc(t_ast *node, int flag)
 	return (open(token->str, flag, 0644));
 }
 
-static void	open_file_error(char *file_name)
+static int	open_file_error(char *file_name)
 {
 	if (!access(file_name, F_OK))
-		ft_putstr_fd("minishell: Permission denied\n", 2);
-	else
+	{
+		if (access(file_name, W_OK | R_OK))
+		{
+			ft_putstr_fd("minishell: Permission denied\n", 2);
+			last_exit_status(1);
+			return (1);
+		}
+		else
+		{
+			last_exit_status(0);
+			return (0);
+		}
+	}
+	else if (!access(file_name, F_OK))
+	{
 		ft_putstr_fd("minishell: No such file or directory\n", 2);
-	last_exit_status(1);
+		last_exit_status(1);
+		return (1);
+	}
+	return (0);
 }
 
 static void	input_redir(t_ast *node)
@@ -64,26 +80,36 @@ static void	input_redir(t_ast *node)
 
 void	handle_redirs(t_ast *node)
 {
-	t_token			*token;
-	int				file;
-	const int		tmp = dup(STDOUT_FILENO);
+	t_token		*token;
+	int			file;
+	const int	tmp = dup(STDOUT_FILENO);
 
 	file = 0;
-	token = node->right->exec->first->content;
 	if (node->type == L_REDIR || node->type == HEREDOC)
 		input_redir(node);
-	else if (node->type == R_REDIR)
+	node->left->type_prev = node->type;
+	if (node->left)
+		execution(node->left);
+	if (node->left && node->left->success == 1)
+	{
+		node->success = 1;
+		return ;
+	}
+	if (node->type == R_REDIR)
 		file = append_trunc(node, TRUN);
 	else if (node->type == APPEND)
 		file = append_trunc(node, APEN);
 	if (file == -1)
+	{
+		node->success = 1;
+		token = node->right->exec->first->content;
 		open_file_error(token->str);
-	else if (node->type == R_REDIR || node->type == APPEND)
+	}
+	if (node->type == R_REDIR || node->type == APPEND)
 	{
 		if (node->type_prev == 0)
 			dup2(file, STDOUT_FILENO);
 		close(file);
-		execution(node->left);
 		dup2(tmp, STDOUT_FILENO);
 	}
 	close (tmp);
