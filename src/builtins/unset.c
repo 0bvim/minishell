d87c6bf79ac97c57afa81a1190442e7af339feb6
Bruno Moretti <bmoretti@c2r3p3.42sp.org.br>
2024-02-19 20:51:11 -0300

    unset leaks fixes

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:50:11 by brmoretti         #+#    #+#             */
-/*   Updated: 2024/02/19 19:36:22 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/02/19 20:50:16 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,22 @@
 void	ft_unsetenv(const char *name)
 {
 	char	**env_vars;
-	char	*temp;
+	char	*env_var;
 	int		i;
 
 	env_vars = environ_holder(NULL, 0);
-	temp = ft_strjoin(name, "=");
-	if (!temp)
-		return ;
 	i = -1;
 	while (env_vars[++i])
 	{
-		if (!ft_strncmp(env_vars[i], temp, ft_strlen(temp)))
+		env_var = key_name(env_vars[i]);
+		if (!ft_strcmp(name, env_var))
 		{
-			free(env_vars[i]);
-			env_vars[i] = ft_strdup("");
+			environ_holder(clone_env_except(env_vars, i), 0);
+			free(env_var);
 			break ;
 		}
+		free(env_var);
 	}
-	free(temp);
-	environ_holder(env_vars, 0);
 }
 
 int	unset(char **args)
--- src/entrance/environ.c
+++ src/entrance/environ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   environ.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brmoretti <brmoretti@student.42.fr>        +#+  +:+       +#+        */
+/*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 11:17:48 by brmoretti         #+#    #+#             */
-/*   Updated: 2024/02/15 22:04:53 by brmoretti        ###   ########.fr       */
+/*   Updated: 2024/02/19 19:57:44 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ char	**environ_holder(char **new_environ, int to_free)
 {
 	static char	**environ_holder;
 
+	if (new_environ && environ_holder)
+		ft_clear_list(&environ_holder);
 	if (new_environ)
 		environ_holder = new_environ;
 	if (environ_holder && to_free)
