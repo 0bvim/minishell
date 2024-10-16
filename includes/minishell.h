2a01defef2577ff5d86af0daa3fb27e38de232b0
vinicius <niviciusdev@gmail.com>
2024-10-16 01:19:29 -0300

    fix: remove print of variable path

--- includes/minishell.h
+++ includes/minishell.h
@@ -14,9 +14,9 @@
 # define MINISHELL_H
 
 /* for read line (compile with -lreadline or just -l) */
-# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+# include <stdio.h>
 
 /* malloc, free, write, printf, getcwd, chdir, stat, lstat and others */
 # include <dirent.h>
@@ -51,7 +51,7 @@ extern volatile int	g_last_signal;
 
 /* enum and struct */
 
-enum	e_token
+enum				e_token
 {
 	ARGUMENT = 1,
 	PIPE,
@@ -71,7 +71,7 @@ enum	e_token
 	EXEC
 };
 
-enum	e_type_match
+enum				e_type_match
 {
 	M_NONE,
 	M_START,
@@ -81,133 +81,134 @@ enum	e_type_match
 
 typedef struct s_token
 {
-  char	*str;
-	int		type;
-	char	next_char;
-}	t_token;
+	char			*str;
+	int				type;
+	char			next_char;
+}					t_token;
 
 typedef struct s_ast
 {
-  struct s_ast		*left;
-  struct s_ast		*right;
-  t_list				*exec;
-  int					type;
-  int					fd;
-}	t_ast;
+	struct s_ast	*left;
+	struct s_ast	*right;
+	t_list			*exec;
+	int				type;
+	int				fd;
+}					t_ast;
 
 typedef struct s_cmd
 {
-  char	**args;
-	char	*cmd;
-}	t_cmd;
+	char			**args;
+	char			*cmd;
+}					t_cmd;
 
-//ENTRANCE
-void		environ_initializer(void);
-char		*static_pwd(char *new_pwd, int to_free);
-char		*static_old_pwd(char *new_pwd, int to_free);
-char		**environ_holder(char **new_environ, int to_free);
-void		term_properties(int restore);
+// ENTRANCE
+void				environ_initializer(void);
+char				*static_pwd(char *new_pwd, int to_free);
+char				*static_old_pwd(char *new_pwd, int to_free);
+char				**environ_holder(char **new_environ, int to_free);
+void				term_properties(int restore);
 
-//BUILTINS
-char		*malloc_pwd(void);
-int			cd(char **args);
-int			builtins_caller(char **args);
-int			echo(char **args);
-int			env(const char **args);
-int			builtin_exit(char **args);
-int			export(char **args);
-int			pwd(const char **args);
-int			unset(char **args);
-void		clear_everything(void);
-char		*key_name(const char *key_value);
+// BUILTINS
+char				*malloc_pwd(void);
+int					cd(char **args);
+int					builtins_caller(char **args);
+int					echo(char **args);
+int					env(const char **args);
+int					builtin_exit(char **args);
+int					export(char **args);
+int					pwd(const char **args);
+int					unset(char **args);
+void				clear_everything(void);
+char				*key_name(const char *key_value);
 
-//prompt
-char		*prompt(void);
-int			is_after_prompt(int is_after);
-void		parser(char *input);
+// prompt
+char				*prompt(void);
+int					is_after_prompt(int is_after);
+void				parser(char *input);
 
-//pipe handling
-int			handle_pipe(t_ast *node_pipe);
+// pipe handling
+int					handle_pipe(t_ast *node_pipe);
 
-//handle redirs
-int			heredoc_substitution(t_list *tokens);
-int			is_redirect_in(int type);
-int			is_redirect_out(int type);
-void		dup_close_tmp(const int *tmp);
-void		handle_redirs(t_ast *node_pipe);
-void		fd_keeper(const int *tmp, int node_fd, int to_close);
-void		dup_fun(t_ast *node, int outin);
+// handle redirs
+int					heredoc_substitution(t_list *tokens);
+int					is_redirect_in(int type);
+int					is_redirect_out(int type);
+void				dup_close_tmp(const int *tmp);
+void				handle_redirs(t_ast *node_pipe);
+void				fd_keeper(const int *tmp, int node_fd, int to_close);
+void				dup_fun(t_ast *node, int outin);
 
-//handle && and ||
-void		handle_and_or(t_ast *node);
+// handle && and ||
+void				handle_and_or(t_ast *node);
 
-//handle block
-void		handle_block(t_token *block);
+// handle block
+void				handle_block(t_token *block);
 
-//expands dollar sign variables
-void		clear_tree(t_ast *root);
+// expands dollar sign variables
+void				clear_tree(t_ast *root);
 
-//AST
-t_ast		*ast_constructor(t_list *tokens);
-int			ast_split_node(t_ast *ast_node, t_list *tokens,
-				t_element *el_to_split);
-t_ast		*ast_holder(t_ast *root, int to_free);
-void		panic_ast(int error, char *msg);
-t_element	*search_and_or(t_list *tokens);
-t_element	*search_pipe(t_list *tokens);
-t_element	*search_redir(t_list *tokens);
+// AST
+t_ast				*ast_constructor(t_list *tokens);
+int					ast_split_node(t_ast *ast_node, t_list *tokens,
+						t_element *el_to_split);
+t_ast				*ast_holder(t_ast *root, int to_free);
+void				panic_ast(int error, char *msg);
+t_element			*search_and_or(t_list *tokens);
+t_element			*search_pipe(t_list *tokens);
+t_element			*search_redir(t_list *tokens);
 
-//EXPANSIONS
-void		expansions(t_list *tokens);
-void		token_expansion(void *p_token);
-void		heredoc_expansion(t_token *token);
-char		**tokens_to_args(t_list *tokens);
-void		asterisk(t_list	*tokens, t_element *el);
+// EXPANSIONS
+void				expansions(t_list *tokens);
+void				token_expansion(void *p_token);
+void				heredoc_expansion(t_token *token);
+char				**tokens_to_args(t_list *tokens);
+void				asterisk(t_list *tokens, t_element *el);
 
 // execve
-void		execution(t_ast *root);
+void				execution(t_ast *root);
 
-//GRAMMAR CHECK
-int			grammar_checker(t_list *tokens);
-int			is_redirect(int type);
-int			is_and_or(int type);
-int			is_quotes(int type);
-int			is_metacharacter(int type);
-int			redir_and_or_pipe_rule(t_element *el);
-int			block_rule(t_element *el);
+// GRAMMAR CHECK
+int					grammar_checker(t_list *tokens);
+int					is_redirect(int type);
+int					is_and_or(int type);
+int					is_quotes(int type);
+int					is_metacharacter(int type);
+int					redir_and_or_pipe_rule(t_element *el);
+int					block_rule(t_element *el);
 
-//UTILS
-int			ft_issymbol(char c);
-void		ft_skip_spaces(const char **str);
-int			which_token(const char *str);
-t_list		*ft_lstsplit(t_list *lst, t_element *el);
-int			last_exit_status(int exit_status);
-void		pid_last_exit_status(pid_t pid);
-void		ft_clear_list(char ***list);
-char		*ft_getenv(const char *name);
-char		*ft_getenv_or_blank(const char *name);
-void		close_fds(void);
-int			is_fork(int status);
-int			ambiguous_error(void);
+// UTILS
+int					ft_issymbol(char c);
+void				ft_skip_spaces(const char **str);
+int					which_token(const char *str);
+t_list				*ft_lstsplit(t_list *lst, t_element *el);
+int					last_exit_status(int exit_status);
+void				pid_last_exit_status(pid_t pid);
+void				ft_clear_list(char ***list);
+char				*ft_getenv(const char *name);
+char				*ft_getenv_or_blank(const char *name);
+void				close_fds(void);
+int					is_fork(int status);
+int					ambiguous_error(void);
 
-//TOKENIZER
-t_list		*tokenizer(const char *str);
-t_list		*token_list_holder(t_list *tokens, int to_free, int to_null);
-void		free_token(void *p_token);
-void		panic_tokenizer(int error, char *msg);
-void		add_token(t_list *tokens, const char **start, const char **mover, \
-	int token_type);
-int			add_special_token(t_list *tokens,
-				const char **start, const char **mover, int token_type);
-int			quotes_validation(const char *str);
-int			parenthesis_validation(const char *str);
-void		trim_edges(void *content);
+// TOKENIZER
+t_list				*tokenizer(const char *str);
+t_list				*token_list_holder(t_list *tokens, int to_free,
+						int to_null);
+void				free_token(void *p_token);
+void				panic_tokenizer(int error, char *msg);
+void				add_token(t_list *tokens, const char **start,
+						const char **mover, int token_type);
+int					add_special_token(t_list *tokens, const char **start,
+						const char **mover, int token_type);
+int					quotes_validation(const char *str);
+int					parenthesis_validation(const char *str);
+void				trim_edges(void *content);
 
-//SIGNALS
-void		signals_initializer(void);
-void		sigint_handler(int signal);
-int			is_after_prompt(int is_after);
-int			on_heredoc(int on_heredoc);
-void		sigquit_case(void);
+// SIGNALS
+void				signals_initializer(void);
+void				sigint_handler(int signal);
+int					is_after_prompt(int is_after);
+int					on_heredoc(int on_heredoc);
+void				sigquit_case(void);
 
 #endif
